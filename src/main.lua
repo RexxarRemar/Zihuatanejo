ec14770b78ba7303401f3355a7a4cc5449ed54fd
luozhongyuang <1289416350@qq.com>
2016-01-06 11:33:39 +0800

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -5,7 +5,7 @@ function __G__TRACKBACK__(errorMessage)
     print(debug.traceback("", 2))
     print("----------------------------------------")
 end
-
+--这是在 github  上面的修改1
 package.path = package.path .. ";src/"
 cc.FileUtils:getInstance():setPopupNotify(false)
 require("app.MyApp").new():run()
